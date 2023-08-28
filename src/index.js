70ea5f995ebc10ba5c3b74abf1ebe8788b550d74
sweeeeetpotato <sweet_potato13@naver.com>
2023-08-28 14:05:39 +0900

    :sparkles: Feat : 리액트 쿼리 설치 및 설정

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(
+const queryClient = new QueryClient();
 
-  <Provider store={store}> 
-    <BrowserRouter>
-      <HelmetProvider>
-        <App tab="home" />
-      </HelmetProvider>
-    </BrowserRouter>  
-  </Provider>
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <HelmetProvider>
+          <App tab='home' />
+        </HelmetProvider>
+      </BrowserRouter>
+    </Provider>
+  </QueryClientProvider>
 );
